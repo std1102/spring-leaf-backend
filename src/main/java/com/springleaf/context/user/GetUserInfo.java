559b72eff5a63a735c30d9dc1e89069a769a9ea0
luyendong1102 <luyendong1102@gmail.com>
2022-07-09 11:11:44 +0700

    1

--- src/main/java/com/springleaf/context/user/GetUserInfo.java
+++ src/main/java/com/springleaf/context/user/GetUserInfo.java
@@ -1,17 +1,33 @@
 package com.springleaf.context.user;
 
 import com.springleaf.annotation.RequestMapping;
+import com.springleaf.common.ErrorCode;
 import com.springleaf.common.RequestType;
+import com.springleaf.object.entity.Login;
+import com.springleaf.object.entity.User;
+import io.ebean.Ebean;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.Map;
 
+@Slf4j
 @RequestMapping(type = RequestType.GET, path = "/user/info")
 public class GetUserInfo extends UserContext{
     @Override
     protected Object _process(Map<String, Object> map) throws IOException, ParseException {
-        result.put("user", userRecord);
+        User user = Ebean.find(User.class).where().eq("id", userRecord.getId()).findOne();
+        Login login = Ebean.find(Login.class).where().eq("user_id", userRecord.getId()).findOne();
+        if (user == null) {
+            return error(ErrorCode.INTERNAL_ERROR);
+        }
+        login.setUser(null);
+        user.setLogin(login);
+        user.getLogin().setPassword(null);
+        user.getLogin().setToken(null);
+        user.setLast_change_password(null);
+        result.put("user", user);
         return result();
     }
 }
